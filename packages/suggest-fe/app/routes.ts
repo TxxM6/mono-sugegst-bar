3f37df2a623b6763a443cefcc44c9336f37a3d95
TxxM6 <tomoki.zenphone2@gmail.com>
2024-12-16 13:19:39 +0900

    add: pages dir

--- packages/suggest-fe/app/routes.ts
+++ packages/suggest-fe/app/routes.ts
@@ -7,10 +7,10 @@ import {
 
 export default [
   ...prefix("suggest", [
-    route("step1", "./suggest/step1/index.page.tsx"),
-    route("step2", "./suggest/step2/index.page.tsx"),
-    route("step3", "./suggest/step3/index.page.tsx"),
-    route("step4", "./suggest/step4/index.page.tsx"),
-    route("step5", "./suggest/step5/index.page.tsx"),
+    route("step1", "./pages/suggest/step1/index.page.tsx"),
+    route("step2", "./pages/suggest/step2/index.page.tsx"),
+    route("step3", "./pages/suggest/step3/index.page.tsx"),
+    route("step4", "./pages/suggest/step4/index.page.tsx"),
+    route("step5", "./pages/suggest/step5/index.page.tsx"),
   ]),
 ] satisfies RouteConfig;
